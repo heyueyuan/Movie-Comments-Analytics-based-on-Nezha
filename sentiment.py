c5518e30b6e8a79f6d5f110875c6c00b0414d18b
Mark he <heyueyuan901@gmail.com>
2019-09-21 20:34:14 -0400

    Add files via upload

---
+++ sentiment.py
@@ -0,0 +1,23 @@
+# encoding : utf-8
+
+from snownlp import SnowNLP 
+import pandas as pd 
+
+data = pd.read_csv("duye_comments.csv")
+
+score = 0
+for d in data['comments']:
+    
+    s = SnowNLP(d)
+    score += s.sentiments
+
+score = score / len(data['comments']) * 100
+
+if score > 80:
+    print("电影评论情感分析为：%d , 非常建议观看" % score)
+else:
+    print("电影评论情感分析为：%d, 不建议观看" % score )
+
+
+
+
